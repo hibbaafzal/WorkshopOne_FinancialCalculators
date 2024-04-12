8dd6d64f2cc48484b8b87e5d93422bd1207bd2f1
hibbaafzal <166542360+hibbaafzal@users.noreply.github.com>
2024-04-12 11:27:52 -0400

    Update Main.java

--- src/main/java/com/ps/Main.java
+++ src/main/java/com/ps/Main.java
@@ -4,37 +4,36 @@ import java.util.*;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) { // main method
 
-        Scanner scanner = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in); 
 
 
         System.out.println("Enter principal: "); //asks user for principal.
-        double loanPrincipal = scanner.nextDouble();
+        double loanPrincipal = scanner.nextDouble(); 
 
         System.out.println("Enter interest rate: "); // asks user for interest rate.
-        double interestRate = scanner.nextDouble();
+        double interestRate = scanner.nextDouble(); 
 
         System.out.println("Enter loan length: "); // asks user for length of loan.
         int loanLength = scanner.nextInt();
 
 
         // monthly interest rate and the total number of payments
-        double monthlyInterestRate = interestRate / 100 / 12; // ask us to display monthly payment
+        double monthlyInterestRate = interestRate / 100 / 12; // calculate monthly interest rate.
 
-        int numPayments = loanLength * 12;
+        int numPayments = loanLength * 12; 
 
         // expected monthly payment
-
         double monthlyPayment = (loanPrincipal * monthlyInterestRate) / (1 - Math.pow(1 + monthlyInterestRate, - numPayments)); // formula for compounded interest
 
         double totalPayment = monthlyPayment * numPayments;
         double totalInterestPaid = totalPayment - loanPrincipal;
 
-        // Output the results
+        // Output the results rounded to 2 decimal places. 
         System.out.printf("Monthly Payment: $%.2f%n", monthlyPayment);
         System.out.printf("Total Interest Paid: $%.2f%n", totalInterestPaid);
     }
 }
 
-// total interest paid
+
